436d95b763ba65e33da389fc719f2b411f236d2a
ManriqueMurillo <159924972+ManriqueMurillo@users.noreply.github.com>
2024-03-23 14:54:00 -0600

    Update ToEmail.py

--- ToEmail.py
+++ ToEmail.py
@@ -7,7 +7,7 @@ import requests
 def send_notification(subject: str, body: str, from_email: str, to_email: str) -> bool:
 	return requests.post(
 		"https://api.mailgun.net/v3/sandbox57bbca6e8fa04dbf881d5afcb0f1aa54.mailgun.org/messages",
-		auth=("api", os.environ["PRIVATE_API_KEY"]),
+		auth=("api", os.environ.get("PRIVATE_API_KEY")),
 		data={"from": from_email,
 			"to": to_email,
 			"subject": subject,
