98b25ae65a2a78e8bbb695226ebd8700381c650b
ManriqueMurillo <159924972+ManriqueMurillo@users.noreply.github.com>
2024-03-23 13:26:27 -0600

    Update ToEmail.py

--- ToEmail.py
+++ ToEmail.py
@@ -10,4 +10,16 @@ def send_notification(subject: str, body: str, from_email: str, to_email: str) -
         response = connection.sendmail(from_addr=from_email, to_addrs=to_email, msg=message)
         print(response)
     return True
+def send_simple_message():
+	return requests.post(
+		"https://api.mailgun.net/v3/sandbox57bbca6e8fa04dbf881d5afcb0f1aa54.mailgun.org/messages",
+		auth=("api", "<PRIVATE_API_KEY>"),
+		data={"from": from_email,
+			"to": to_email,
+			"subject": subject,
+			"text": body})
 
+# You can see a record of this email in your logs: https://app.mailgun.com/app/logs.
+
+# You can send up to 300 emails/day from this sandbox server.
+# Next, you should add your own domain so you can send 10000 emails/month for free.
