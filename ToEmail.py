9610acf33fdba30a1c30108bc3e19b468ed7b9c2
ManriqueMurillo <159924972+ManriqueMurillo@users.noreply.github.com>
2024-03-23 15:09:28 -0600

    Update ToEmail.py

--- ToEmail.py
+++ ToEmail.py
@@ -5,13 +5,15 @@ import requests
 
 
 def send_notification(subject: str, body: str, from_email: str, to_email: str):
-	return requests.post(
+	message=requests.post(
 		"https://api.mailgun.net/v3/sandbox57bbca6e8fa04dbf881d5afcb0f1aa54.mailgun.org/messages",
 		auth=("api", os.environ.get("PRIVATE_API_KEY")),
 		data={"from": from_email,
 			"to": to_email,
 			"subject": subject,
 			"text": body})
+	print(message)
+	return message
 
 # You can see a record of this email in your logs: https://app.mailgun.com/app/logs.
 
